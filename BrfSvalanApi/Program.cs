0cb31a513da88a051fe8469a93a37ac2e637ac76
Bambi på hal is <kronosjo.arvid@live.com>
2023-10-15 00:56:16 +0200

    update test

--- BrfSvalanApi/Program.cs
+++ BrfSvalanApi/Program.cs
@@ -1,6 +1,7 @@
 using BrfSvalanApi;
 using Iot.Device.CharacterLcd;
 using Iot.Device.Mcp23xxx;
+using Iot.Device.Pcx857x;
 using System.Device.Gpio;
 using System.Device.I2c;
 
@@ -41,22 +42,33 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
-using I2cDevice i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
-using Mcp23008 serialDriver = new Mcp23008(i2cDevice);
-using var lcd = new Lcd1602(dataPins: new int[] { 0, 1, 2, 3 },
-                        registerSelectPin: 4,
-                        readWritePin: 5,
-                        enablePin: 6,
-                        controller: new GpioController(PinNumberingScheme.Logical, serialDriver));
-lcd.Clear();
-lcd.BacklightOn = true;
-for(int i = 0; i < 10; i++)
-{
-    lcd.BacklightOn = i % 2 == 0;
-    Thread.Sleep(1000);
-}
-lcd.SetCursorPosition(0, 0);
-lcd.Write($"Hi from wellsb.com");
+//using I2cDevice i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+//using Mcp23008 serialDriver = new Mcp23008(i2cDevice);
+//using var lcd = new Lcd1602(dataPins: new int[] { 0, 1, 2, 3 },
+//                        registerSelectPin: 4,
+//                        readWritePin: 5,
+//                        enablePin: 6,
+//                        controller: new GpioController(PinNumberingScheme.Logical, serialDriver));
+//lcd.Clear();
+//lcd.BacklightOn = true;
+//for(int i = 0; i < 10; i++)
+//{
+//    lcd.BacklightOn = i % 2 == 0;
+//    Thread.Sleep(1000);
+//}
+//lcd.SetCursorPosition(0, 0);
+//lcd.Write($"Hi from wellsb.com");
+using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+using var driver = new Pcf8574(i2c);
+using var lcd = new Lcd1602(registerSelectPin: 0,
+                        enablePin: 2,
+                        dataPins: new int[] { 4, 5, 6, 7 },
+                        backlightPin: 3,
+                        backlightBrightness: 0.1f,
+                        readWritePin: 1,
+                        controller: new GpioController(PinNumberingScheme.Logical, driver));
+
+
 
 app.Run();
 
